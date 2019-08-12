25aa7f9686f908684aab417b328036703e879291
AntoineCartier <antoinecartier.contact@gmail.com>
2019-08-12 15:31:25 +0200

    Ask/answer - Try1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  # def ask
+  #   @ask = input
+  # end
+
+  # def answer
+  #   @answer_1 = "Great!"
+  #   @answer_2 = "Silly question, get dressed and go to work!"
+  #   @answer_3 = "I don't care, get dressed and go to work!"
+  #   if
+  #     @ask == 'I am going to work'
+  #     return @answer_1
+  #   elsif @ask.incude? '?'
+  #     return @answer_2
+  #   else @answer_3
+  #   end
+  # end
 end
